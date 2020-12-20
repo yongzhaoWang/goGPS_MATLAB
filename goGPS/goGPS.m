2c147bd073e05b88ccdf56fd73ee8409eee3fca6
Andrea <gatti.andrea@gmail.com>
2020-12-20 14:48:25 +0000

    Name the log with the name of the project

--- goGPS/goGPS.m
+++ goGPS/goGPS.m
@@ -154,7 +154,7 @@ function goGPS(ini_settings, use_gui, flag_online)
     if core.state.isLogOnFile()
         log.newLine();
         log.enableFileOut();
-        log.setOutFile(fullfile(core.state.getOutDir, 'log', 'goGPS_run_${NOW}.log')); % <= to enable project logging
+        log.setOutFile(fullfile(core.state.getOutDir, 'log', sprintf('goGPS_run_%s_${NOW}.log', strrep(core.state.getPrjName, ' ', '_')))); % <= to enable project logging
         % log.setOutFile(); <= to enable system logging (save into system folder)
         log.disableFileOut();
         fnp = File_Name_Processor();
