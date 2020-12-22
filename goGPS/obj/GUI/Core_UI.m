ea52c6b3b61040524a3757d4ef668c1409dc7a48
Andrea <gatti.andrea@gmail.com>
2020-12-22 23:11:21 +0000

    Manage double yy axis in beautify

--- goGPS/obj/GUI/Core_UI.m
+++ goGPS/obj/GUI/Core_UI.m
@@ -614,7 +614,14 @@ classdef Core_UI < Logos
                     ax.YLabel.Color = [0.8 0.8 0.8];
                     ax.ZLabel.Color = [0.8 0.8 0.8];
                     ax.XColor = [0.8 0.8 0.8];
-                    ax.YColor = [0.8 0.8 0.8];
+                    if numel(ax.YAxis) == 2
+                        yyaxis left
+                        ax.YColor = [0.8 0.8 0.8];
+                        yyaxis right
+                        %ax.YColor = [0.8 0.2 0.2];
+                    else
+                        ax.YColor = [0.8 0.8 0.8];
+                    end
                     ax.ZColor = [0.8 0.8 0.8];
                     text_label = findall(ax, 'Type', 'text');
                     for txt = text_label(:)'
@@ -686,7 +693,14 @@ classdef Core_UI < Logos
                     ax.YLabel.Color = 1-[0.8 0.8 0.8];
                     ax.ZLabel.Color = 1-[0.8 0.8 0.8];
                     ax.XColor = 1-[0.8 0.8 0.8];
-                    ax.YColor = 1-[0.8 0.8 0.8];
+                    if numel(ax.YAxis) == 2
+                        yyaxis left
+                        ax.YColor = 1-[0.8 0.8 0.8];
+                        yyaxis right
+                        %ax.YColor = [0.8 0.2 0.2];
+                    else
+                        ax.YColor = 1-[0.8 0.8 0.8];
+                    end
                     ax.ZColor = 1-[0.8 0.8 0.8];
                     text_label = findall(ax, 'Type', 'text');
                     for txt = text_label(:)'
